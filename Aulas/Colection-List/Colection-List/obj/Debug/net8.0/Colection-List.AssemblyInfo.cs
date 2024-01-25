9a910d32aacc819d9d007ec7f5287940db3286dd
miguel.t <127998017+migueltotti@users.noreply.github.com>
2024-01-25 11:14:11 -0300

    Ajustes

--- Aulas/Colection-List/Colection-List/obj/Debug/net8.0/Colection-List.AssemblyInfo.cs
+++ Aulas/Colection-List/Colection-List/obj/Debug/net8.0/Colection-List.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Colection-List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0644f6cce8cca77a579920aa24adbeeb70397edf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Colection-List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Colection-List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
