2ed623cde471b1417671207439dfcb50e3be62ff
Miguel Totti <migueltotti2005@gmail.com>
2024-04-17 15:45:56 -0300

    Listas
    implementacao de Listas, operacoes e funcoes basicas

--- Udemy/Section 6 - Comportamento de memoria, arrays, listas/Exercicios/Exercicio_1/Program.cs
+++ Udemy/Section 6 - Comportamento de memoria, arrays, listas/Exercicios/Exercicio_1/Program.cs
@@ -14,6 +14,8 @@
             string email;
             int room;
 
+            // utilizando ordenacao
+            /*
             for(int i = 0; i < n; i++)
             {
                 Console.WriteLine($"Rent #{i+1}:");
@@ -35,6 +37,32 @@
             for (int i = 0; i < n; i++)
             {
                 Console.WriteLine(quartos[i].Room + ": " + quartos[i]);
+            }*/
+
+            // alocando os quartos no indice do vetor correspondente ao 'room' inserido
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Rent #{i + 1}:");
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+                Console.Write("Email: ");
+                email = Console.ReadLine();
+                Console.Write("Room: ");
+                room = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                quartos[room] = new Quarto { Name = name, Email = email, Room = room };
+            }
+
+            Console.WriteLine("Busy rooms:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (quartos[i] != null)
+                {
+                    Console.WriteLine(quartos[i].Room + ": " + quartos[i]);
+                }
             }
         }
     }
---
+++ Udemy/Section 6 - Comportamento de memoria, arrays, listas/List/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("Isadora");
+            list.Add("Miguel");
+            list.Add("Frank");
+            list.Add("Macaco");
+
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            list.Insert(2, "Kikii");
+
+            Console.WriteLine("\n----------------------\n");
+
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n----------------------\n");
+
+            Console.WriteLine("Quantidade de elemetos: " + list.Count);
+
+            Console.WriteLine("Primeiro elemento com a primeira letra M: " + list.Find(x => x[0] == 'M'));
+
+            Console.WriteLine("Ultimo elemento com a primeira letra M: " + list.FindLast(x => x[0] == 'M'));
+
+            Console.WriteLine("Index do primeiro elemento com a primeira letra I: " + list.FindIndex(x => x[0] == 'I'));
+
+            Console.WriteLine("Index do ultimo elemento com a primeira letra M: " + list.FindLastIndex(x => x[0] == 'M'));
+
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+
+            Console.WriteLine("\n----------------------\n");
+            foreach(string item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n----------------------\n");
+            list.Remove("Miguel");
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\n");
+
+            list.RemoveAll(x => x[0] == 'M');
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\n");
+
+        }
+    }
+}
