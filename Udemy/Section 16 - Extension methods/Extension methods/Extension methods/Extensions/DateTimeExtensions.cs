f01344186c4f78f3062d233a62d6d2193276f45d
Miguel Totti <migueltotti2005@gmail.com>
2024-05-13 23:25:41 -0300

    Extension methods

---
+++ Udemy/Section 16 - Extension methods/Extension methods/Extension methods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    internal static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if(duration.TotalHours < 24)
+            {
+                return duration.TotalHours.ToString("F1", System.Globalization.CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", System.Globalization.CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
---
+++ Udemy/Section 16 - Extension methods/Extension methods/Extension methods/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Extension_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2024, 05, 11, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            Console.WriteLine();
+
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
